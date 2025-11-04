572156bcd23e21214849392c4d1ad22eacea2c35
Alix Desramé <ab.desrame@icloud.com>
2025-11-04 15:53:22 +0100

    fini

--- messenger.py
+++ messenger.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import json
+
 
 server = {
     'users': [
@@ -20,6 +22,9 @@ server = {
 }
 
 
+
+
+
 def menuprincipal():
     print('=== Messenger ===')
     print('x. Leave')
@@ -29,6 +34,55 @@ def menuprincipal():
     print('a.ajout users')
     print('p.menu principal')
     choice = input('Select an option: ')
+    if choice == 'x':
+     print('Bye!')
+    if choice == 'u':
+     afficher_users()
+    if choice == 'g':
+     afficher_groupes()
+    if choice == 'm':
+     afficher_messages()
+    if choice == 'p':
+     menuprincipal()
+    if choice == 'a':
+     add_users()
+    if choice == 'g':
+     add_groupes()
+    else:
+     print('Unknown option:', choice)
+
+def afficher_users():
+   for i in range (len(server['users'])):
+      print(server['users'][i]['id'],server['users'][i]['name'])
+   menuprincipal()
+   
+def afficher_groupes():
+   for i in range (len(server['channels'])):
+      print(server['channels'][i]['id'],server['channels'][i]['name'],server['channels'][i]['member_ids'])
+   menuprincipal()
+   
+def afficher_messages():
+   for i in range (len(server['messages'])):
+      print(server['messages'][i]['sender_id'], server['messages'][i]['content'])
+   menuprincipal()
+   
+def add_users():
+   newid= input('id nouvel utilisateur: ')
+   newname= input('nom nouvel utilisateur: ')
+   newuser= {'id': newid, 'name': newname}
+   server['users'].append(newuser)
+   afficher_users()
+   menuprincipal()
+   
+def add_groupes():
+   newidg= input('id nouveau groupe: ')
+   newnameg= input('nom nouveau groupe: ')
+   afficher_users()
+   newmb= input('membres du groupe: ')
+   newchannel= {'id': newidg, 'name': newnameg, 'member_ids': [newmb]}
+   server['channels'].append(newchannel)
+   afficher_groupes()
+   menuprincipal()
 
 
 print('=== Messenger ===')
@@ -42,26 +96,18 @@ choice = input('Select an option: ')
 if choice == 'x':
     print('Bye!')
 if choice == 'u':
-    for i in range (len(server['users'])):
-      print(server['users'][i]['id'],server['users'][i]['name'])
+    afficher_users()
 if choice == 'g':
-    for i in range (len(server['channels'])):
-      print(server['channels'][i]['id'],server['channels'][i]['name'],server['channels'][i]['member_ids'])
+    afficher_groupes()
 if choice == 'm':
-    for i in range (len(server['messages'])):
-      print(server['messages'][i]['sender_id'], server['messages'][i]['content'])
+    afficher_messages()
 if choice == 'p':
    menuprincipal()
 if choice == 'a':
-   newid= input('id nouvel utilisateur: ')
-   newname= input('nom nouvel utilisateur: ')
-   newuser= {'id': newid, 'name': newname}
-   server['users'].append(newuser)
-   for i in range (len(server['users'])):
-      print(server['users'][i]['id'],server['users'][i]['name'])
+   add_users()
+if choice == 'g':
+   add_groupes()
 else:
     print('Unknown option:', choice)
-
-
 
 
